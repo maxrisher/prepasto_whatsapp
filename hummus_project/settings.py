7bb37272506e90ba674020cca92d643173baad30
maxrisher <max.risher@gmail.com>
2024-08-17 08:11:50 -0600

    typo

--- hummus_project/settings.py
+++ hummus_project/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = [os.getenv('PUBLIC_DOMAIN')]
+ALLOWED_HOSTS = [os.getenv('RAILWAY_PUBLIC_DOMAIN')]
 
 # Application definition
 
