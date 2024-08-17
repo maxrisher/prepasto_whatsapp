88ec75e384f121bc85c4efa828157979c6876e8d
maxrisher <max.risher@gmail.com>
2024-08-17 14:22:13 -0600

    pass off post requests to the webhook to lambda

--- whatsapp_bot/views.py
+++ whatsapp_bot/views.py
@@ -22,24 +22,13 @@ def webhook(request):
             return HttpResponse('Forbidden', status=403)
     
     if request.method == 'POST':
-        data = json.loads(request.body)
-
-        #get the message details
-        try:
-            message = data['entry'][0]['changes'][0]['value']['messages'][0]
-            sender = message['from']
-            text = message['text']['body']
-
-            # generate a response to the message
-            response = process_message(text)
-
-            # send the response back
-            send_whatsapp_message(sender, response)
-
-        except KeyError:
-            # expected data is not here!
-            pass
-
+        lambda_client = boto3.client('lambda')
+        lambda_client.invoke(
+            FunctionName='',
+            InvocationType='Event',
+            Payload=request.body
+        )
+    
     return HttpResponse('OK', status=200)
 
 def process_message(received_text):
