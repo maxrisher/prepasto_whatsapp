500115aaf1bd80670c1b154e3012c7e5f148ca4a
maxrisher <max.risher@gmail.com>
2024-08-20 16:27:49 -0600

    created custom user models which do not require usernames. users have phone and timezone fields though

--- custom_users/models.py
+++ custom_users/models.py
@@ -1,8 +1,73 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-class CustomUser(AbstractUser):
-    pass
+from django.conf import settings
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, phone=None, time_zone=None):
+        """
+        Creates and saves a User with the given email, phone, time zone, and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            phone=phone,
+            time_zone=time_zone,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, phone=None, time_zone=None):
+        """
+        Creates and saves a superuser with the given email, phone, time zone, and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            phone=phone,
+            time_zone=time_zone,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    # we want these in the following format: +17204768288
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    #This will default to our application's default TIME_ZONE unless a session calls timezone.activate()
+    time_zone = models.CharField(max_length=50, default=settings.TIME_ZONE)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
