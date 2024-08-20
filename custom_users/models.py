5038bb2c3d6d2dfb5f9a9932ec483c764d231df2
maxrisher <max.risher@gmail.com>
2024-08-20 16:37:34 -0600

    removed phone and tz from user creation

--- custom_users/models.py
+++ custom_users/models.py
@@ -4,32 +4,28 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.conf import settings
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, phone=None, time_zone=None):
+    def create_user(self, email, password=None):
         """
-        Creates and saves a User with the given email, phone, time zone, and password.
+        Creates and saves a User with the given email, and password.
         """
         if not email:
             raise ValueError("Users must have an email address")
         
         user = self.model(
             email=self.normalize_email(email),
-            phone=phone,
-            time_zone=time_zone,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, email, password=None, phone=None, time_zone=None):
+    def create_superuser(self, email, password=None):
         """
-        Creates and saves a superuser with the given email, phone, time zone, and password.
+        Creates and saves a superuser with the given email, and password.
         """
         user = self.create_user(
             email,
             password=password,
-            phone=phone,
-            time_zone=time_zone,
         )
         user.is_admin = True
         user.save(using=self._db)
