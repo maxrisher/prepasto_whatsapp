bb9c25bfcdbd5e83e79257907e3ce740ac2e3c33
maxrisher <max.risher@gmail.com>
2024-08-27 15:00:45 -0600

    load .env variables for pytest testing

--- hummus_project/settings.py
+++ hummus_project/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
