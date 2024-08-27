f087a14d1374132cad5e3196cf188b1aca33f961
maxrisher <max.risher@gmail.com>
2024-08-27 09:38:39 -0400

    url conf for lambda webhook added

--- whatsapp_bot/urls.py
+++ whatsapp_bot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("webhook/", views.webhook, name="webhook")
+    path("webhook/", views.webhook, name="webhook"),
+    path("lambda_webhook/", views.food_processing_lambda_webhook, name = 'lambda-webhook')
 ]
