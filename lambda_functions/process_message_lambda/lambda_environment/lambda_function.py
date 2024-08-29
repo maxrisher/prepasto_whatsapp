71a162a5cbc0d3fe786aefc6b76177ce78a1f30b
Max Risher <max.risher@gmail.com>
2024-08-29 09:30:58 -0600

    changed whatsapp phone number for sending

--- lambda_functions/process_message_lambda/lambda_environment/lambda_function.py
+++ lambda_functions/process_message_lambda/lambda_environment/lambda_function.py
@@ -20,6 +20,7 @@ def send_whatsapp_message(recipient, message):
         "text": {"body": message},
     }
 
+    #The Whatsapp api url contains the phone number that we use to send messages to users
     response = requests.post(os.getenv('WHATSAPP_API_URL'), headers=headers, json=data)
     return response.json()
 
