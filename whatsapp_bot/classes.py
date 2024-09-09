f11f205b3c12158eaa113fa506efdcfbd9ea2fd9
Max Risher <max.risher@gmail.com>
2024-09-09 14:37:23 -0700

    typos

--- whatsapp_bot/classes.py
+++ whatsapp_bot/classes.py
@@ -23,7 +23,7 @@ class PayloadFromWhatsapp:
         self.whatsapp_wa_id = str(self.request_dict["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"])
 
     def get_or_create_whatsapp_user_in_dj_db(self):
-        whatsapp_user, user_was_created = WhatsappUser.objects.get_or_create(phone_number=user_wa_id, whatsapp_id=user_wa_id)
+        whatsapp_user, user_was_created = WhatsappUser.objects.get_or_create(phone_number=self.whatsapp_wa_id, whatsapp_id=self.whatsapp_wa_id)
 
         self.prepasto_whatsapp_user_object = whatsapp_user
         self.is_message_from_new_user = user_was_created
@@ -53,8 +53,8 @@ class PayloadFromWhatsapp:
             return False
     
     def get_whatsapp_text_message_data(self):
-        self.whatsapp_text_message_text = str(request_body_dict["entry"][0]['changes'][0]['value']['messages'][0]['text']['body'])
-        self.whatsapp_message_id = str(request_body_dict["entry"][0]["changes"][0]["value"]["messages"][0]["id"])
+        self.whatsapp_text_message_text = str(self.request_dict["entry"][0]['changes'][0]['value']['messages'][0]['text']['body'])
+        self.whatsapp_message_id = str(self.request_dict["entry"][0]["changes"][0]["value"]["messages"][0]["id"])
 
     def notify_message_sender_of_processing(self):
         send_whatsapp_message(self.whatsapp_wa_id, "I got your message and I'm calculating the nutritional content!")
