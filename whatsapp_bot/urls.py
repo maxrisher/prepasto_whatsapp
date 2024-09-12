51111ab94e925aa1660433a820b9e6710efaa496
Max Risher <max.risher@gmail.com>
2024-09-11 18:57:57 -0600

    quick fix to make whatsapp happy with our URLs

--- whatsapp_bot/urls.py
+++ whatsapp_bot/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("whatsapp_webhook/", views.listens_for_whatsapp_cloud_api_webhook, name="whatsapp-webhook"),
+    path("webhook/", views.listens_for_whatsapp_cloud_api_webhook, name="whatsapp-webhook"),
     path("lambda_webhook/", views.food_processing_lambda_webhook, name = 'lambda-webhook')
 ]
