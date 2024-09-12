cc1cccc19b4170a702f392679e9a113f91532eb5
Max Risher <max.risher@gmail.com>
2024-09-11 18:13:01 -0600

    remove old methods from utils

--- whatsapp_bot/utils.py
+++ whatsapp_bot/utils.py
@@ -30,68 +30,6 @@ def send_whatsapp_message(recipient_phone_number, message):
     logger.warning(response.json())
     return response.json()
 
-@transaction.atomic
-def send_meal_whatsapp_message(recipient_phone_number, meal_id):
-    #STEP 1: Craft a message
-    # fetch our site's whatsapp user object
-    prepasto_user, whatsapp_user_created = WhatsappUser.objects.get_or_create(whatsapp_wa_id='14153476103')
-
-    new_meal = Meal.objects.get(id=meal_id)
-
-    button_dict = {
-        "type": "button",
-        "body": {
-        "text": new_meal.text_summary
-        },
-        "action": {
-            "buttons": [
-                {
-                "type": "reply",
-                "reply": {
-                        "id": str(new_meal.id),
-                        "title": settings.MEAL_DELETE_BUTTON_TEXT
-                    }
-                }
-            ]
-        }
-    }
-    
-    headers = {
-        "Authorization": f"Bearer {os.getenv('WHATSAPP_TOKEN')}",
-        "Content-Type": "application/json",
-    }
-
-    data = {
-        "messaging_product": "whatsapp",
-        "recipient_type": "individual",
-        "to": recipient_phone_number, #Testing with manually input phone number
-        "type": "interactive",
-        "interactive": button_dict,
-    }
-
-    logger.info("Request:")
-    logger.info(headers)
-    logger.info(data)
-
-    #STEP 2: send our message
-    response = requests.post(os.getenv('WHATSAPP_API_URL'), headers=headers, json=data)
-
-    logger.info("Response:")
-    logger.info(response)
-    logger.info(response.json())
-
-    #Step 3: add our sent message to the database
-    dict_response = response.json()
-    sent_message_id = dict_response['messages'][0]['id']
-    logger.info(dict_response)
-    logger.info(dict_response['messages'])
-
-    sent_message = WhatsappMessage.objects.create(whatsapp_user=prepasto_user,
-                                                  whatsapp_message_id=sent_message_id,
-                                                  content=new_meal.text_summary,
-                                                  direction='OUT',
-                                                  meal=new_meal)
-
 def send_to_lambda(request_body_dict):
     json_payload = json.dumps(request_body_dict)
 
@@ -106,36 +44,6 @@ def send_to_lambda(request_body_dict):
         Qualifier=os.getenv('LAMBDA_ALIAS')
     )
     return
-
-# A) Extracts all meal information from labda dict 
-# B) creates a Meal object for this 
-# C) adds it to a custom_user's diary
-# Returns the diary and meal.
-def add_meal_to_db(dict_from_lambda, custom_user):
-    meal_totals = dict_from_lambda.get('total_nutrition')
-    calories = round(meal_totals.get('calories', 0))
-    fat = round(meal_totals.get('fat', 0))
-    carbs = round(meal_totals.get('carbs', 0))
-    protein = round(meal_totals.get('protein', 0))
-
-    logger.info(custom_user)
-    logger.info(custom_user.current_date)
-
-    user_local_date = custom_user.current_date
-    
-    diary, created = Diary.objects.get_or_create(custom_user=custom_user, local_date=user_local_date)
-
-    new_meal = Meal.objects.create(
-        custom_user=custom_user,
-        diary=diary,
-        calories=calories,
-        carbs=carbs,
-        fat=fat,
-        protein=protein,
-        local_date=user_local_date,
-    )
-
-    return diary, new_meal
 
 # This finds a meal object referenced by a user and deletes it
 # The database operations here are all or nothing
