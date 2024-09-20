58d8192d7e0a64711075809756dfe677d63d8ff2
Max Risher <max.risher@gmail.com>
2024-09-20 16:12:58 -0600

    validator added

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MinValueValidator
 
 from whatsapp_bot.models import WhatsappUser
 
