e6cf07e55002015791b06bc3ed60e8fee02337c2
Max Risher <max.risher@gmail.com>
2024-10-21 11:55:01 -0700

    cleaned up output from new diary processing

--- lambda_functions/process_message_lambda/lambda_environment/dish.py
+++ lambda_functions/process_message_lambda/lambda_environment/dish.py
@@ -10,6 +10,7 @@ class Dish:
     def __init__(self, llm_dish_dict: Dict):
         # Attributes to set on initialization
         self.llm_dish_dict = llm_dish_dict
+
         self.name = llm_dish_dict.get('name')
         self.usual_ingredients = llm_dish_dict.get('usual_ingredients')
         self.state = llm_dish_dict.get('state')
@@ -20,10 +21,6 @@ class Dish:
         self.brand_name = llm_dish_dict.get('manufactured_by')
         self.chain_restaurant = llm_dish_dict.get('chain_restaurant')
 
-        # Meta attributes
-        self.llm_responses: Dict[str, str] = {}
-        self.errors: List[str] = []
-
         # Attributes set while processing
         self.is_generic_dish: bool = True
         self.nutrients_per_100g = {'calories_per_100g': 0,
@@ -31,9 +28,12 @@ class Dish:
                                 'fat_per_100g': 0,
                                 'protein_per_100g': 0}
         self.prepasto_usda_code: str = None
+        self.fndds_categories: List[str] = []
+        self.usda_food_data_central_food_name: str = None
+        self.usda_food_data_central_id: str = None
         self.nutrition_citation_website: str = None
         self.web_portion_reference_csv: str = None
-        self.grams = 0
+        self.grams: float = 0
         self.nutrition = {'calories': 0,
                                 'carbs': 0,
                                 'fat': 0,
@@ -51,6 +51,11 @@ class Dish:
             searcher = UsdaNutrientSearcher(self.llm_dish_dict)
             await searcher.search()
             self.prepasto_usda_code = searcher.prepasto_usda_code
+
+            self.fndds_categories = searcher.fndds_categories
+            self.usda_food_data_central_food_name = searcher.usda_food_data_central_food_name
+            self.usda_food_data_central_id = searcher.usda_food_data_central_id
+            self.nutrition_citation_website = 'USDA'
 
         else:
             searcher = WebNutrientSearcher(self.llm_dish_dict)
@@ -85,123 +90,22 @@ class Dish:
             'protein': calc_nutrient(self.nutrients_per_100g.get('protein_per_100g', 0)),
         })
 
-
-dish_schema = {
-  "$id": "https://thalos.fit/dish.schema.json",
-  "$schema": "https://json-schema.org/draft/2020-12/schema",
-  "title": "Dish",
-  "type": "object",
-  "properties": {
-    "name": {
-      "type": "string",
-      "description": "A short and general description of the food. If we're lucky, there will be a FNDDS dish with the same name. E.g., 'Shepherd's pie'."
-    },
-    "usual_ingredients": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "description": "Ingredients often found in the generic version of the dish. Helps match the dish to a FNDDS food based on ingredients lists. E.g., ['ground lamb', 'onions', 'carrots', etc.]."
-    },
-    "state": {
-      "type": "string",
-      "description": "Words describing how the food has been prepared, including the degree of preparation, cooking method, or preservation method. E.g., 'cooked, sauteed (meat and vegetable filling), boiled and mashed (potato topping), baked (entire pie)'."
-    },
-    "qualifiers": {
-      "type": ["array"],
-      "items": {
-        "type": "string"
-      },
-      "description": "Any user-provided information on the overall nutritional content of a dish. Can be empty if no information is provided. E.g., 'sugar-free', 'full-fat', 'high-protein', etc."
-    },
-    "confirmed_ingredients": {
-      "type": ["array"],
-      "items": {
-        "type": "string"
-      },
-      "description": "Any user-provided details on the ingredients present in the dish. Allows the system to store additional user-provided information about the dish's ingredients. E.g., ['lamb'] or ['beef', 'cheddar']."
-    },
-    "amount": {
-      "type": "string",
-      "description": "Useful information about the amount of the dish consumed, including any detail about quantities of dish ingredients. E.g., '3 cups of shepherd's pie. 1 cup of the pie was mashed potatoes', '369 g total', 'Not specified'."
-    },
-    "similar_dishes": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "description": "Common but nutritionally similar dishes that can be used as a fallback if the dish is missing from the database. Dishes should be common, have similar ingredients, similar macronutrient ratios, caloric density, and physical density. E.g., ['moussaka', 'irish stew', 'meat pie', etc.]."
-    },
-    "llm_responses": {
-      "type": "object",
-      "additionalProperties": {
-        "type": "string"
-      },
-      "description": "Dictionary of responses from the language model, with keys and string values"
-    },
-    "errors": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "description": "List of errors encountered during processing"
-    },
-    "candidate_thalos_ids": {
-      "type": "object",
-      "properties": {
-        "fndds_category_search_results": {
-          "type": "array",
-          "items": {
-            "type": "integer"
-          }
-        },
-        "fndds_and_sr_legacy_google_search_results": {
-          "type": "array",
-          "items": {
-            "type": "integer"
-          }
-        }
-      },
-      "description": "List of candidate food codes for the dish."
-    },
-    "matched_thalos_id": {
-      "type": ["integer"],
-      "description": "The matched food code"
-    },
-    "usda_food_data_central_id": {
-      "type": ["integer", "null"],
-      "description": "USDA Food Data Central ID for the matched food, or null if not found."
-    },
-    "usda_food_data_central_food_name": {
-      "type": ["string"],
-      "description": "USDA Food Data Central name for the matched food"
-    },
-    "grams": {
-      "type": ["integer"],
-      "description": "The weight of the dish in grams, or null if not specified"
-    },
-    "nutrition": {
-      "type": ["object"],
-      "additionalProperties": {
-        "type": "integer"
-      },
-      "description": "A dictionary containing nutritional information, where keys are nutrient names and values are nutrient quantities"
-    },
-    "fndds_categories": {
-      "type": "array",
-      "items": {
-        "type": "integer"
-      },
-      "description": "FNDDS categories matched for the dish."
-    },
-    "google_search_queries_usda_site": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "description": "List of Google search queries used to search the USDA site."
+    def to_full_dict(self):
+        return {
+        'name': self.name,
+        'usual_ingredients': self.usual_ingredients,
+        'state': self.state,
+        'qualifiers': self.qualifiers,
+        'confirmed_ingredients': self.confirmed_ing,
+        'amount': self.amount,
+        'similar_dishes': self.similar_dishes,
+        'brand_name': self.brand_name,
+        'chain_restaurant': self.chain_restaurant,
+        'fndds_categories': self.fndds_categories,
+        'prepasto_usda_code': self.prepasto_usda_code,
+        'usda_food_data_central_id': self.usda_food_data_central_id,
+        'usda_food_data_central_food_name': self.usda_food_data_central_food_name,
+        'nutrition_citation_website': self.nutrition_citation_website,
+        'grams': self.grams,
+        'nutrition': self.nutrition,
     }
-  },
-  "required": ["name", "usual_ingredients", "state", "qualifiers", "confirmed_ingredients", "amount", "similar_dishes", "llm_responses", "errors", "candidate_thalos_ids", "matched_thalos_id", "usda_food_data_central_id", "usda_food_data_central_food_name", "grams", "nutrition", "fndds_categories", "google_search_queries_usda_site"],
-  "additionalProperties": False
-}
--- lambda_functions/process_message_lambda/lambda_environment/meal.py
+++ lambda_functions/process_message_lambda/lambda_environment/meal.py
@@ -1,7 +1,7 @@
 from typing import List, Dict
 import asyncio
 
-from dish import Dish, dish_schema
+from dish import Dish
 from llm_caller import LlmCaller
 
 class Meal:
@@ -9,8 +9,6 @@ class Meal:
     self.description = description
     self.dishes: List[Dish] = None
     self.total_nutrition: Dict[str, float] = None
-    self.errors: List[str] = []
-    self.llm_responses: Dict[str, str] = {}
 
   async def process(self):
     await self._create_dishes()
@@ -57,55 +55,4 @@ class Meal:
       "description": self.description,
       "dishes": [dish.to_full_dict() for dish in self.dishes],
       "total_nutrition": self.total_nutrition,
-      "errors": self.errors,
-      "llm_responses": self.llm_responses
     }
-
-
-meal_schema = {
-  "$id": "https://thalos.fit/meal.schema.json",
-  "$schema": "https://json-schema.org/draft/2020-12/schema",
-  "title": "Meal",
-  "type": "object",
-  "properties": {
-    "description": {
-      "type": "string",
-      "description": "A description of the meal. This is typically user-provided input that will be processed to extract dish information."
-    },
-    "dishes": {
-      "type": "array",
-      "items": dish_schema,
-      "description": "A list of Dish objects that are part of this meal."
-    },
-    "total_nutrition": {
-      "type": "object",
-      "description": "The total nutrition information for the meal, with nutrients as keys and their amounts (in grams, calories) as values.",
-      "additionalProperties": {
-        "type": "number",
-        "description": "The amount of a nutrient, such as calories, protein, fat, carbohydrates, etc."
-      }
-    },
-    "errors": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "description": "A list of any errors encountered during the processing of the meal or its dishes."
-    },
-    "llm_responses": {
-      "type": "object",
-      "description": "Responses from the language model from dish extraction."
-    }
-  },
-  "required": ["description", "dishes", "total_nutrition", "errors", "llm_responses"],
-  "additionalProperties": False,
-}
-
-import os
-import sys
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
-
-meal = Meal("One banana, one rossopomodoro margerita pizza")
-asyncio.run(meal.process())
-print("done!")
